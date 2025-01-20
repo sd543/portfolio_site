621068cb0b54813032e6a0130c2ae5c5b3683df6
emerson <sd543tv@gmail.com>
2025-01-20 14:45:12 -0800

    added AOS/modals

--- PortfolioSite/wwwroot/js/scripts.js
+++ PortfolioSite/wwwroot/js/scripts.js
@@ -5,3 +5,11 @@
         target.scrollIntoView({ behavior: 'smooth' });
     });
 });
+
+function openModal(id) {
+    document.getElementById(id).style.display = 'flex';
+}
+
+function closeModal(id) {
+    document.getElementById(id).style.display = 'none';
+}
