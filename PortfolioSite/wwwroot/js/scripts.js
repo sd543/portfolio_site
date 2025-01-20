a617180a1b1d584fb371eee9cd2ee0e740cd4185
emerson <sd543tv@gmail.com>
2025-01-20 13:35:11 -0800

    init files for site template

---
+++ PortfolioSite/wwwroot/js/scripts.js
@@ -0,0 +1,7 @@
+﻿document.querySelectorAll('a').forEach(link => {
+    link.addEventListener('click', e => {
+        e.preventDefault();
+        const target = document.querySelector(link.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
