4abc2689b70d426adb293ed15d52a121571d1b22
emerson <sd543tv@gmail.com>
2025-01-20 16:30:10 -0800

    fixed double-scrollbar and background not displaying

--- PortfolioSite/wwwroot/js/scripts.js
+++ PortfolioSite/wwwroot/js/scripts.js
@@ -1,8 +1,9 @@
-﻿document.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', e => {
+﻿document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', e => {
         e.preventDefault();
-        const target = document.querySelector(link.getAttribute('href'));
-        target.scrollIntoView({ behavior: 'smooth' });
+        document.querySelector(anchor.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
     });
 });
 
@@ -13,3 +14,12 @@ function openModal(id) {
 function closeModal(id) {
     document.getElementById(id).style.display = 'none';
 }
+
+
+
+AOS.init({
+    duration: 1000, // Animation duration
+    easing: 'ease-in-out', // Smoother easing
+    once: true, // Animations occur only once
+    mirror: false, // Prevent duplicate animations on scrolling back up
+});
